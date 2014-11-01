c8dd1c62abde7e3434bee682222a7ba0c492e879
sonjapeterson <sonja.peterson@gmail.com>
2014-11-01 04:47:40 -0400

    Updated destination method to return strings

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -1,8 +1,6 @@
 module CitiesHelper
 	def find_destination(places, num_results)
 		center = Geocoder::Calculations.geographic_center(places)
-		binding.pry
-		puts center
 		distances = []
 		City.all.each do |city|
 			distance = Geocoder::Calculations.distance_between(center, [city.latitude, city.longitude])
@@ -13,8 +11,8 @@ module CitiesHelper
 			end
 
 		end
-		binding.pry
 		distances = distances.sort_by{|element| element[:distance]}
-		distances[0..num_results].map{|i| i[:id] }
+		distances = distances[0..num_results-1].map{|i| i[:id] }
+		distances.map{|id| City.find(id).city_state}
 	end
 end
