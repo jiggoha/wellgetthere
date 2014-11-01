7dcaf49a18330767b5569382916e096782383361
Jiaao Hou <jiaao.hou@yale.edu>
2014-11-01 12:05:25 -0400

    dont sleep

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -8,18 +8,18 @@ class CitiesController < ApplicationController
 		Incomings.all.each do |incoming|
 			@locations.push(incoming.text)
 		end
+		Incomings.all.destroy
+		Yo.all.destroy
 
 		@resultingPlaces = find_destination(@locations, 3)
 		@client = GroupMe::Client.new(:token => ENV['ACCESS_TOKEN'])
 		@counter = 1
 		if !@resultingPlaces.empty?
 			@resultingPlaces.each do |nameOfPlace|
-				sleep(15)
+				sleep(2)
 				@client.create_message(GROUP_ID, "Calculated option number " + @counter.to_s + ": " + nameOfPlace + "\n")
 				@counter += 1
 			end
 		end
-		Incomings.all.destroy
-		Yo.all.destroy
 	end
 end
