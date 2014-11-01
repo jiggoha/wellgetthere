f5ce1b91be0aec6cdf6b59fb72f68e0b878cca78
sonjapeterson <sonja.peterson@gmail.com>
2014-11-01 15:46:41 -0400

    Attempting to troubleshoot Geocoder issues

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -9,7 +9,7 @@ module CitiesHelper
 		center = Geocoder::Calculations.geographic_center(coordinates)
 		distances = []
 		City.all.each do |city|
-			distance = Geocoder::Calculations.distance_between(center, [city.latitude, city.longitude])
+			distance = distance_between(center, [city.latitude, city.longitude])
 			if !distance.nan?
 				distances << {id: city.id, distance: distance}
 			else
@@ -21,4 +21,30 @@ module CitiesHelper
 		distances = distances[0..num_results-1].map{|i| i[:id] }
 		distances.map{|id| City.find(id).city_state}
 	end
+
+	 def distance_between(point1, point2)
+
+	  # convert degrees to radians
+	  point1 = to_radians(point1)
+	  point2 = to_radians(point2)
+
+	  # compute deltas
+	  dlat = point2[0] - point1[0]
+	  dlon = point2[1] - point1[1]
+
+	  a = (Math.sin(dlat / 2))**2 + Math.cos(point1[0]) *
+	      (Math.sin(dlon / 2))**2 * Math.cos(point2[0])
+	  c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1-a))
+	  c * 6371.0
+	end
+
+	def to_radians(*args)
+      args = args.first if args.first.is_a?(Array)
+      if args.size == 1
+        args.first * (Math::PI / 180)
+      else
+        args.map{ |i| to_radians(i) }
+      end
+    end
+
 end
