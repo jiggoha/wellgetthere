d7fd41431a05e29dbb56007c6b93a447ffd4e221
Jiaao Hou <jiaao.hou@yale.edu>
2014-11-01 01:36:42 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'bot#index'
   get 'bot/callback' => 'bot#callback'
+  post 'bot/callback' => 'bot#callback'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
