e32b4dd8038fc367b5c6ebbf0ded0cc312d13f6e
Jiaao Hou <jiaao.hou@yale.edu>
2014-11-01 10:59:41 -0400

    yay callbacks

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -2,48 +2,47 @@ class BotController < ApplicationController
 
 	def index
 		
-		# @client = GroupMe::Client.new(:token => ENV['ACCESS_TOKEN'])
-		# people_count = @client.group(ENV['GROUP_ID']).members.count
+		@client = GroupMe::Client.new(:token => ENV['ACCESS_TOKEN'])
+		people_count = @client.group(ENV['GROUP_ID']).members.count
 
-		# not_all_replied = true
-		# replies = 0
-		# @locations = []
-		# @yoUserNames = []
+		not_all_replied = true
+		replies = 0
+		@locations = []
+		@yoUserNames = []
 		
+		while(not_all_replied)
+			not_meaningful_message = true
+			@messages_number = Incomings.count
 
-		# while(not_all_replied)
-		# 	not_meaningful_message = true
-		# 	@messages_number = Incomings.count
+			while(not_meaningful_message)
 
-		# 	while(not_meaningful_message)
+				if(@messages_number < Incomings.count)
+					message = Incomings.first.text
+					if message.split()[0] == '/location'
+						@locations.push(message.split()[1..-1].join(' '))
+						replies += 1
+						Incomings.first.destroy
 
-		# 		if(@messages_number < Incomings.count)
-		# 			message = Incomings.first.text
-		# 			if message.split()[0] == '/location'
-		# 				@locations.push(message.split()[1..-1].join(' '))
-		# 				replies += 1
-		# 				Incomings.first.destroy
-
-		# 				if replies == 1 and !@yoUserNames.empty?
-		# 					uri = URI('https://api.justyo.co/yo/')
-		# 					@yoUserNames.each do |yoUserName|
-		# 						Net::HTTP.post_form(uri, api_token: ENV['YO_API_KEY'], username: yoUserName)
-		# 					end
-		# 				end
-		# 				if replies == people_count
-		# 					not_all_replied = false
-		# 				end
-		# 			elsif message.split()[0] == '/yo'
-		# 				@yoUserNames.push(message.split()[1..-1].join(' '))
-		# 				Incomings.first.destroy
-		# 			else
-		# 				Incomings.first.destroy
-		# 			end
-		# 				not_meaningful_message = false
-		# 		end
-		# 	end
-		# end
-		# redirect_to :controller => 'cities', :action => 'index', :locations => @locations
+						if replies == 1 and !@yoUserNames.empty?
+							uri = URI('https://api.justyo.co/yo/')
+							@yoUserNames.each do |yoUserName|
+								Net::HTTP.post_form(uri, api_token: ENV['YO_API_KEY'], username: yoUserName)
+							end
+						end
+						if replies == people_count
+							not_all_replied = false
+						end
+					elsif message.split()[0] == '/yo'
+						@yoUserNames.push(message.split()[1..-1].join(' '))
+						Incomings.first.destroy
+					else
+						Incomings.first.destroy
+					end
+						not_meaningful_message = false
+				end
+			end
+		end
+		redirect_to :controller => 'cities', :action => 'index', :locations => @locations
 	end
 
 	def callback
--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,16 +2,16 @@ class CitiesController < ApplicationController
 	include CitiesHelper
 
 	def index
-		# @locations = params[:locations]	
+		@locations = params[:locations]	
 		
-		# @resultingPlaces = find_destination(@locations, 3)
-		# @client = GroupMe::Client.new(:token => ACCESS_TOKEN)
-		# @counter = 1
-		# if !@resultingPlaces.empty?
-		# 	@resultingPlaces.each do |nameOfPlace|
-		# 		@client.create_message(GROUP_ID, "Calculated option number " + @counter.to_s + ": " + nameOfPlace + "\n")
-		# 		@counter += 1
-		# 	end
-		# end
+		@resultingPlaces = find_destination(@locations, 3)
+		@client = GroupMe::Client.new(:token => ACCESS_TOKEN)
+		@counter = 1
+		if !@resultingPlaces.empty?
+			@resultingPlaces.each do |nameOfPlace|
+				@client.create_message(GROUP_ID, "Calculated option number " + @counter.to_s + ": " + nameOfPlace + "\n")
+				@counter += 1
+			end
+		end
 	end
 end
