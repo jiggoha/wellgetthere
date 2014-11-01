d7d051fd466004bab8ac4fff778e2515310ec05c
Ebstein <stanislaw.swidwinski@yale.edu>
2014-11-01 04:20:35 -0400

    Yo added

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -19,28 +19,25 @@ class BotController < ApplicationController
 			@messages_number = @client.messages_count(GROUP_ID)
 
 			while(not_meaningful_message)
-				if(@messages_number < @client.messages_count(GROUP_ID))
 
+				if(@messages_number < @client.messages_count(GROUP_ID))
 					message = @client.messages(GROUP_ID)[0][:text]
 					if message.split()[0] == '/location'
 						@locations.push(message.split()[1..-1].join(' '))
 
 						replies += 1
 
-						if replies == 1 and !@yoUserNames.null?
+						if replies == 1 and !@yoUserNames.empty?
 							uri = URI('https://api.justyo.co/yo/')
 							@yoUserNames.each do |yoUserName|
 								Net::HTTP.post_form(uri, api_token: YO_API_KEY, username: yoUserName)
 							end
-							
-						end
-							
 						end
 						if replies == people_count
 							not_all_replied = false
 						end
 					elsif message.split()[0] == '/yo'
-						@yoUserNames.push(message.split()[1..-1].join(' '))						
+						@yoUserNames.push(message.split()[1..-1].join(' '))	
 					end
 						not_meaningful_message = false
 				end
