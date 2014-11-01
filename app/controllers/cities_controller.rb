873e1e94286295f6181cc6d1eddf02d801a9575c
Jiaao Hou <jiaao.hou@yale.edu>
2014-11-01 12:19:29 -0400

    i think we are almost there

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -8,18 +8,24 @@ class CitiesController < ApplicationController
 			Incomings.all.each do |incoming|
 				@locations.push(incoming.text)
 			end
-			Incomings.all.destroy
-			Yo.all.destroy
 
-			# @resultingPlaces = find_destination(@locations, 3)
-			# @client = GroupMe::Client.new(:token => ENV['ACCESS_TOKEN'])
-			# @counter = 1
-			# if !@resultingPlaces.empty?
-			# 	@resultingPlaces.each do |nameOfPlace|
-			# 		sleep(2)
-			# 		@client.create_message(ENV['GROUP_ID'], "Calculated option number " + @counter.to_s + ": " + nameOfPlace + "\n")
-			# 		@counter += 1
-			# 	end
-			# end
+			Incomings.all.each do |i|
+				i.destroy
+			end
+
+			Yo.all.each do |y|
+				y.destroy
+			end
+
+			@resultingPlaces = find_destination(@locations, 3)
+			@client = GroupMe::Client.new(:token => ENV['ACCESS_TOKEN'])
+			@counter = 1
+			if !@resultingPlaces.empty?
+				@resultingPlaces.each do |nameOfPlace|
+					sleep(2)
+					@client.create_message(ENV['GROUP_ID'], "Calculated option number " + @counter.to_s + ": " + nameOfPlace + "\n")
+					@counter += 1
+				end
+			end
 	end
 end
