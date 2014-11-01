371e3870dbaf17229a34ded381a5e1aa094c9383
Jiaao Hou <jiaao.hou@yale.edu>
2014-11-01 12:12:52 -0400

    test

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,23 +3,23 @@ class CitiesController < ApplicationController
 
 	def index
 
-		@locations = []
+			# @locations = []
 
-		Incomings.all.each do |incoming|
-			@locations.push(incoming.text)
-		end
-		Incomings.all.destroy
-		Yo.all.destroy
+			# Incomings.all.each do |incoming|
+			# 	@locations.push(incoming.text)
+			# end
+			# Incomings.all.destroy
+			# Yo.all.destroy
 
-		@resultingPlaces = find_destination(@locations, 3)
-		@client = GroupMe::Client.new(:token => ENV['ACCESS_TOKEN'])
-		@counter = 1
-		if !@resultingPlaces.empty?
-			@resultingPlaces.each do |nameOfPlace|
-				sleep(2)
-				@client.create_message(GROUP_ID, "Calculated option number " + @counter.to_s + ": " + nameOfPlace + "\n")
-				@counter += 1
-			end
-		end
+			# @resultingPlaces = find_destination(@locations, 3)
+			# @client = GroupMe::Client.new(:token => ENV['ACCESS_TOKEN'])
+			# @counter = 1
+			# if !@resultingPlaces.empty?
+			# 	@resultingPlaces.each do |nameOfPlace|
+			# 		sleep(2)
+			# 		@client.create_message(GROUP_ID, "Calculated option number " + @counter.to_s + ": " + nameOfPlace + "\n")
+			# 		@counter += 1
+			# 	end
+			# end
 	end
 end
