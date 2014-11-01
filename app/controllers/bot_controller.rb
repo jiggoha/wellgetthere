62b557a1185377add0fc36298a0b87ed0743d908
Jiaao Hou <jiaao.hou@yale.edu>
2014-11-01 02:38:44 -0400

    Giving up

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -5,33 +5,32 @@ class BotController < ApplicationController
 	BOT_ID_JIMGYM = "53a81761fcf30e3146a9957c5a"
 
 	def index
-		
-		 #@client = GroupMe::Client.new(:token => ACCESS_TOKEN)
+		@client = GroupMe::Client.new(:token => ACCESS_TOKEN)
 
-		# noMeaningfulMessage = true
-		# while(noMeaningfulMessage)
-		# 	noNewMessage = true
-		# 	@messages_number = @client.messages_count(GROUP_ID)
+		noMeaningfulMessage = true
+		while(noMeaningfulMessage)
+			noNewMessage = true
+			@messages_number = @client.messages_count(GROUP_ID)
 
-		# 	while(noNewMessage)
-		# 		if(@messages_number < @client.messages_count(GROUP_ID))
-		# 			if @client.messages(GROUP_ID)[0][:text] == '/test'
-		# 				noMeaningfulMessage = false
-		# 			end
-		# 				noNewMessage = false
-		# 		end
-		# 	sleep(0.5)
-		# 	end
-		# end
+			while(noNewMessage)
+				if(@messages_number < @client.messages_count(GROUP_ID))
+					if @client.messages(GROUP_ID)[0][:text] == '/test'
+						noMeaningfulMessage = false
+					end
+						noNewMessage = false
+				end
+			sleep(0.5)
+			end
+		end
 	end
 
 	def callback
-		@text = params[:text]
-		if @text == '/test'
-			redirect_to :controller => 'bot', :action => 'index'
-		else 
-			sleep(1)
-			redirect_to :controller => 'bot', :action => 'callback'
-		end
+		# @text = params[:text]
+		# if @text == '/test'
+		# 	redirect_to :controller => 'bot', :action => 'index'
+		# else 
+		# 	sleep(1)
+		# 	redirect_to :controller => 'bot', :action => 'callback'
+		# end
 	end
 end
